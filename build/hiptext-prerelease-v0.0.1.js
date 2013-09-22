a425e16f076e3e7483365bfc58e5794ee4b2c7df
Hiptext JS <hiptextjs@gmail.com>
2013-09-22 22:05:57 +0100

    New Version (prerelease-0.0.3)
    Finally does something. Replaces raw symbols with their HTML & symbols

--- build/hiptext-prerelease-v0.0.1.js
+++ build/hiptext-prerelease-v0.0.1.js
@@ -1,4 +1,4 @@
-// Hiptext Version 0.0.2 (PRERELEASE)
+// Hiptext Version 0.0.3 (PRERELEASE)
 //Copyright (c) 2013 Hiptext JS
 
 //Permission is hereby granted, free of charge, to any person obtaining a copy of
@@ -18,10 +18,58 @@
 //IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 //CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-(function() {
-    var Hiptext = {
-        parse: function(code) {
-            return code;
-        }
-    };
-})();
+var sortSymbols = (function(data) {
+	var codes = [
+		//Math Symbols
+		['?', '&forall;'],
+		['?', '&part;'],
+		['?', '&exist;'],
+		['Ø', '&empty;'],
+		['?', '&nabla;'],
+		['?', '&isin;'],
+		['?', '&notin;'],
+		['?', '&ni;'],
+		['?', '&prod;'],
+		['?', '&sum;'],
+		['-', '&minus;'],
+		['*', '&lowast;'],
+		['v', '&radic;'],
+		['?', '&prop;'],
+		['8', '&infin;'],
+		['?', '&ang;'],
+		['?', '&and;'],
+		['?', '&or;'],
+		['n', '&cap;'],
+		['?', '&cup;'],
+		['?', '&int;'],
+		['?', '&there4;'],
+		['~', '&sim;'],
+		['?', '&cong;'],
+		['˜', '&asymp;'],
+		['?', '&ne;'],
+		['=', '&equiv;'],
+		['=', '&le;'],
+		['=', '&ge;'],
+		['?', '&sub;'],
+		['?', '&sup;'],
+		['?', '&nsub;'],
+		['?', '&sube;'],
+		['?', '&supe;'],
+		['?', '&oplus;'],
+		['?', '&otimes;'],
+		['?', '&perp;'],
+		['·', '&sdot;']
+		//More Symbols Coming Soon
+	];
+	var temp = data
+	for (var i = 0; i < codes.length; i++) {
+	    temp = temp.replace(codes[i][0], codes[i][1]);
+	}
+	return temp;
+});
+
+var Hiptext = {
+    parse: function(code) {
+        code = sortSymbols(code);
+    }
+};
